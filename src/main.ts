3655a4baca402e4a7ccf5770303f6a7b0a3de21d
Andres Diago <andresdiag@unicauca.edu.co>
2024-05-16 16:06:20 -0500

    :construction: Clase 21. Cómo evitar parámetros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove properties that do not have decorators
+      forbidNonWhitelisted: true, // throw an error if properties are not whitelisted
+      transform: true // transform payloads to DTO instances
+    })
+  )
   await app.listen(3000)
 }
 bootstrap()
